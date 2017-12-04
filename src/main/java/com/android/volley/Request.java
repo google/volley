fc8ff0423fe9a3af6595088b34f912fb2d920ffb
Navid Ht <navid.htb@gmail.com>
2017-12-05 03:20:16 +0330

    Log fix (#112)
    * Fix finding "caller" in VolleyLog
    
    "trace[i].getClass()" always returns StackTraceElement, the class of
    "trace[i]" itself. Method getClassName() is what really needed here to
    get the class of element in the call stack trace.
    Because "clazz" was alwasy StackTraceElement so the condition
    "if (!clazz.equals(VolleyLog.class.getName()))" was always true. But
    code worked with no problem because loop started from index 2
    (int i = 2) which luckily is always the method called VolleyLog.
    
    * Fix call to MarkerLog.finish() in Request
    
    The "this" in run() referes to the anonymous Runnable containing it. The
    intention here was to calling the toString() of the Request class.

--- src/main/java/com/android/volley/Request.java
+++ src/main/java/com/android/volley/Request.java
@@ -249,7 +249,7 @@ public abstract class Request<T> implements Comparable<Request<T>> {
                     @Override
                     public void run() {
                         mEventLog.add(tag, threadId);
-                        mEventLog.finish(this.toString());
+                        mEventLog.finish(Request.this.toString());
                     }
                 });
                 return;
