50151ab54cabb33a88bf06ab0879a29f92737882
Jeff Davidson <jpd@google.com>
2018-04-04 18:49:47 -0700

    Add missing null checks to fix unit tests.

--- src/main/java/com/android/volley/internal/HeaderConversions.java
+++ src/main/java/com/android/volley/internal/HeaderConversions.java
@@ -3,6 +3,7 @@ package com.android.volley.internal;
 import com.android.volley.Header;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
@@ -16,6 +17,12 @@ public final class HeaderConversions {
     private HeaderConversions() {}
 
     public static Map<String, String> toHeaderMap(List<Header> allHeaders) {
+        if (allHeaders == null) {
+            return null;
+        }
+        if (allHeaders.isEmpty()) {
+            return Collections.emptyMap();
+        }
         Map<String, String> headers = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
         // Later elements in the list take precedence.
         for (Header header : allHeaders) {
@@ -25,6 +32,12 @@ public final class HeaderConversions {
     }
 
     public static List<Header> toAllHeaderList(Map<String, String> headers) {
+        if (headers == null) {
+            return null;
+        }
+        if (headers.isEmpty()) {
+            return Collections.emptyList();
+        }
         List<Header> allHeaders = new ArrayList<>(headers.size());
         for (Map.Entry<String, String> header : headers.entrySet()) {
             allHeaders.add(new Header(header.getKey(), header.getValue()));
