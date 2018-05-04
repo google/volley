fd949dd083d3fe56a5b66c7070cb5697ec384e69
Dylan Chong <dylan.chong@icloud.com>
2018-05-04 19:59:17 +1200

    WIP Added body and body content type

--- src/main/java/com/android/volley/toolbox/BuildableRequest.java
+++ src/main/java/com/android/volley/toolbox/BuildableRequest.java
@@ -1,8 +1,13 @@
 package com.android.volley.toolbox;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.NetworkResponse;
 import com.android.volley.Request;
 import com.android.volley.Response;
+
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
 
 /**
  * TODO
@@ -11,20 +16,26 @@ public class BuildableRequest<T> extends Request<T> {
 
     private final Object mLock = new Object();
     private final ResponseParser<T> parser;
+    private final String bodyContentType;
+    private final byte[] body;
 
-    private Response.Listener<T> mListener;
+    private Response.Listener<T> mListener; // Mutable to be consistent with other Requests
 
     public BuildableRequest(
             int method,
             String url,
             Response.Listener<T> listener,
             Response.ErrorListener errorListener,
-            ResponseParser<T> parser
+            ResponseParser<T> parser,
+            String bodyContentType,
+            byte[] body
     ) {
         super(method, url, errorListener);
         // TODO Null checks for listeners
         this.mListener = listener;
         this.parser = parser;
+        this.bodyContentType = bodyContentType;
+        this.body = body;
     }
 
     @Override
@@ -48,6 +59,32 @@ public class BuildableRequest<T> extends Request<T> {
         }
         if (listener != null) {
             listener.onResponse(response);
+        }
+    }
+
+    @Override
+    public String getBodyContentType() {
+        return bodyContentType;
+    }
+
+    @Override
+    public byte[] getBody() throws AuthFailureError {
+        return body;
+    }
+}
+
+/**
+ * TODO make methods for all of the other kinds of stuff
+ * TODO desc
+ * TODO move
+ */
+class Body {
+    public static byte[] forJSONObject(JSONObject jsonObject) {
+        try {
+            return jsonObject.toString().getBytes(JsonRequest.PROTOCOL_CHARSET);
+        } catch (UnsupportedEncodingException e) {
+            throw new Error();
+            // TODO
         }
     }
 }
