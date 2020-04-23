a42e3748d8afb08312e57a34e5ac9e81227e30ff
Julien Biral <jbiral@google.com>
2020-04-23 14:52:11 -0700

    Add extra Nullable annotation to AutFailureError

--- src/main/java/com/android/volley/AuthFailureError.java
+++ src/main/java/com/android/volley/AuthFailureError.java
@@ -23,6 +23,7 @@ import androidx.annotation.Nullable;
 @SuppressWarnings("serial")
 public class AuthFailureError extends VolleyError {
     /** An intent that can be used to resolve this exception. (Brings up the password dialog.) */
+    @Nullable
     private Intent mResolutionIntent;
 
     public AuthFailureError() {}
