cea3b7f69d03783da4bc2b03bc95cc3905b0e478
NicolasDagnas <ndagnas@live.fr>
2020-04-29 10:42:30 +0200

    Fixing Google Java Format

--- src/main/java/com/android/volley/toolbox/HurlStack.java
+++ src/main/java/com/android/volley/toolbox/HurlStack.java
@@ -181,8 +181,8 @@ public class HurlStack extends BaseHttpStack {
      * @param length size of response stream.
      * @return an InputStream object for read response.
      */
-    protected InputStream createInputStream(Request<?> request, HttpURLConnection connection,
-                                             int responseCode, int length) {
+    protected InputStream createInputStream(
+            Request<?> request, HttpURLConnection connection, int responseCode, int length) {
         return new UrlConnectionInputStream(connection);
     }
 
@@ -324,8 +324,8 @@ public class HurlStack extends BaseHttpStack {
      * @return an OutputStream to which the request body will be written.
      * @throws IOException
      */
-    protected OutputStream createOutputStream(Request<?> request, HttpURLConnection connection,
-                                               int length) throws IOException {
+    protected OutputStream createOutputStream(
+            Request<?> request, HttpURLConnection connection, int length) throws IOException {
         return connection.getOutputStream();
     }
 }
