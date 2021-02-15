28a10da9f259700c816096f49c2ed571b5da81fb
Jeff Davidson <jpd@google.com>
2021-02-15 11:16:42 -0800

    Publish SNAPSHOT builds to OSSRH instead of OJO.
    Example build (generated locally): https://oss.sonatype.org/#nexus-search;gav~com.android.volley~volley~~~
    
    See #394

--- publish-snapshot-on-commit.sh
+++ publish-snapshot-on-commit.sh
@@ -5,11 +5,11 @@ GITHUB_BRANCH=${GITHUB_REF#refs/heads/}
 if [ "$GITHUB_REPOSITORY" == "google/volley" ] && \
    [ "$GITHUB_EVENT_NAME" == "push" ] && \
    [ "$GITHUB_BRANCH" == "master" ]; then
-  echo -e "Publishing snapshot build to OJO...\n"
+  echo -e "Publishing snapshot build...\n"
 
-  ./gradlew artifactoryPublish
+  ./gradlew publish
 
-  echo -e "Published snapshot build to OJO"
+  echo -e "Published snapshot build"
 else
   echo -e "Not publishing snapshot"
 fi
